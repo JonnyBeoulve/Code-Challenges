fe19a0fd12536f2f054ad8ff13338e182ba7d626
Jonathan <jleack@gmail.com>
2018-08-13 12:40:16 -0700

    Reverse string

---
+++ ReverseString/reversestring.js
@@ -0,0 +1,18 @@
+/*=======================================================================
+// This function will simply reverse a string in JavaScript.
+=======================================================================*/
+reverseString = (str) => {
+    let reversedString = '';
+
+    if (str.length < 2) return undefined;
+
+    for (let i = str.length - 1; i >= 0; i--) reversedString = reversedString + str[i];
+
+    return reversedString;
+}
+
+// Execute reverseString and log the returned string
+console.log(reverseString('Hello World!'));
+
+// Test disallowed string length
+console.log(reverseString('H'));
