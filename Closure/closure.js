5964c374dd1400ae18c48982ed0808d360e3613a
Jonathan <jleack@gmail.com>
2018-08-13 13:15:49 -0700

    Closure

---
+++ Closure/closure.js
@@ -0,0 +1,17 @@
+/*=======================================================================
+// This function will return a reference to a function that contains
+// a closure used for outputting a concatenated first and last name.
+=======================================================================*/
+fullName = (fName, lName) => {
+    // Concatenate passed-in names
+    let fullName = `${fName} ${lName}`;
+    
+    // This closure accesses a variable outside its block scope
+    let outputFullName = () => console.log(fullName);
+
+    return outputFullName;
+}
+
+// Create instance and then call it
+let jonny = fullName('Jonny', 'Beoulve');
+jonny();
