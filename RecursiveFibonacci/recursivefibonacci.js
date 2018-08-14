c54188675812c9bab07f3909f13607d377575ffa
Jonathan <jleack@gmail.com>
2018-08-13 18:07:59 -0700

    Recursive fibonacci

---
+++ RecursiveFibonacci/recursivefibonacci.js
@@ -0,0 +1,14 @@
+/*=======================================================================
+// This function will perform a fibonacci sequence for n length using
+// recursion.
+=======================================================================*/
+recursiveFibonacci = (num) => {
+    if (num === 1) return [0, 1];
+    else {
+        let tempNum = recursiveFibonacci(num - 1);
+        tempNum.push(tempNum[tempNum.length - 1] + tempNum[tempNum.length - 2]);
+        return tempNum;
+    } 
+};
+
+ console.log(recursiveFibonacci(8));
