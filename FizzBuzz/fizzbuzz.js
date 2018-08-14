35b95d2038ea76aa9b8d5e5254b652d1cde44d30
Jonathan <jleack@gmail.com>
2018-08-14 11:12:50 -0700

    Anyone want a little FIZZBUZZ?

---
+++ FizzBuzz/fizzbuzz.js
@@ -0,0 +1,21 @@
+/*=======================================================================
+// This quintessential coding challenge problem asks the user the 
+// following:
+// "Write a program that prints the numbers from 1 to 100. But for 
+// multiples of three print “Fizz” instead of the number and for the 
+// multiples of five print “Buzz”. For numbers which are multiples of 
+// both three and five print “FizzBuzz”."
+=======================================================================*/
+fizzBuzz = () => {
+    console.log("==== Begin Fizz Buzz ====")
+    for (let i = 1; i <= 100; i++) {
+        if (i % 15 === 0) console.log(`${i}. Fizz Buzz`);
+        else if (i % 3 === 0) console.log(`${i}. Fizz`);
+        else if (i % 5 === 0) console.log(`${i}. Buzz`);
+        else console.log(`${i}.`);
+    }
+    console.log("=========================")
+}
+
+// Call fizzBuzz
+fizzBuzz();
