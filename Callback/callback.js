b6cec5b20e1d5232b41dcb13a0163c75d3693a17
Jonathan <jleack@gmail.com>
2018-08-15 16:33:44 -0700

    Callback

---
+++ Callback/callback.js
@@ -0,0 +1,21 @@
+/*=======================================================================
+// In this example we'll clarify what a callback is using a simple
+// array of strings. Our strings will be SpaceX mission names.
+=======================================================================*/
+let missions = [
+    "Hispasat 30W-6",
+    "Iridium NEXT Mission 5",
+    "CRS-14",
+    "TESS",
+    "Bangabandhu-1",
+    "Iridium NEXT Mission 6",
+    "SES-12",
+    "CRS-15",
+    "Telstar 19V",
+    "Iridium NEXT Mission 7"
+]
+
+// For every mission in the missions array, perform a callback function
+// outputting the name of the mission. Note that in this instance ForEach is
+// the higher order function, while the console.log is the callback function.
+missions.forEach((mission, index) => { console.log(`\n${index + 1}. ${mission}`) });
